b2df908f5eea275c2afadca2cd6e072038b1b682
ustithegod <ustithegod@email.com>
2025-08-18 16:47:09 +0300

    2.14 complete

---
+++ 2.14/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func or(channels ...<-chan interface{}) chan interface{} {
+	res := make(chan interface{})
+	for _, ch := range channels {
+		go func() {
+			for {
+				if _, ok := <-ch; !ok {
+					break
+				}
+			}
+			close(res)
+		}()
+	}
+	return res
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(3*time.Second),
+		sig(5*time.Second),
+		sig(1*time.Minute),
+	)
+	fmt.Printf("done after %v", time.Since(start))
+}
